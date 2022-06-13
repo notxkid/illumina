fdc6016c0e5367ccad0c8a8967626456c0360f11
xkidhehe <96038811+notxkid@users.noreply.github.com>
2022-06-13 14:18:38 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,12 @@
+-- Loader.lua
+
+getgenv()["IrisAd"] = true
 local Notification = loadstring(game:HttpGet("https://api.irisapp.ca/Scripts/IrisBetterNotifications.lua"))()
 
 if game.PlaceId == 855499080 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/notxkid/illumina/main/main/skywarsv2.lua',true))()
+elseif game.PlaceId == 4913581664 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/notxkid/illumina/main/main/cartride.lua',true))()
 else
     Notification.Notify("Illumina Hub", "Game is not supported", {Duration = 2,Main={Rounding = true}})
 end
