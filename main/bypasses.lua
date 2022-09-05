3daa27dd2fd6755a79666797e1570c36445201c7
xkidhehe <96038811+notxkid@users.noreply.github.com>
2022-09-05 10:58:24 -0500

    Update bypasses.lua

--- main/bypasses.lua
+++ main/bypasses.lua
@@ -11,6 +11,17 @@ old = hookmetamethod(game, "__index", function(...)
     return old(...)
 end)
 
+local OldNameCall = nil
+OldNameCall = hookmetamethod(game, "__namecall", function(Self, ...)
+    local NameCallMethod = getnamecallmethod()
+
+    if tostring(string.lower(NameCallMethod)) == "kick" then
+        return nil
+    end
+    
+    return OldNameCall(Self, ...)
+end)
+
 for _, con in ipairs(getconnections(lp.Character.Humanoid.Changed)) do
     con:Disable()
 end
